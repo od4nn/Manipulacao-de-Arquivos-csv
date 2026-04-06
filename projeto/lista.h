6849948436608316b2732511c9fcd79078d08ecf
Daniel Gama <danielgmbr30@gmail.com>
2026-04-06 12:10:35 -0300

    feat: criando header do projeto, colocando defines e funcoes

--- projeto/lista.h
+++ projeto/lista.h
@@ -2,6 +2,23 @@
 #ifndef MANIPULACAO_ARQUIVOS_LISTA_H
 #define MANIPULACAO_ARQUIVOS_LISTA_H
 
-//defines e funcoes aqui
+/* -- DEFINES -- */
+
+#define QUANT_ESTADOS 26
+#define TAM_LINHA 200 //mais que suficiente para cada linha dos arquivos das bases de dados
+
+
+typedef enum {
+    OK = 0, //sucesso geral
+
+    /* Codigos de erro */
+
+    ERR_ABRIR_ARQUIVO = -1, //erro fopen
+
+}Codigos_Erro;
+
+/* -- DECLARACAO DE FUNCOES -- */
+
+int concatenar_dados(void);
 
 #endif //MANIPULACAO_ARQUIVOS_LISTA_H
